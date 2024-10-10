6deb1563663c5517ac2614de5a33f2b0969fdb67
SamH477 <huhns@etown.edu>
2024-10-09 20:03:09 -0400

    Start creating file to handle + display JSON data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,30 @@
 import './App.css';
 import Navbar from './Navbar';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import GolfDeals from './json-handler/handle_data';  // Import the GolfDeals component
 
 function App() {
 
   return (
-    <>
+    <Router>
       <Navbar />
-    </>
+      <Routes>
+        {/* Home Route */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Link to="/deals">
+                <button className="button-deals">Go to Golf Deals</button>
+              </Link>
+            </>
+          }
+        />
+        {/* Golf Deals Route */}
+        <Route path="/deals" element={<GolfDeals />} />
+      </Routes>
+    </Router>
   );
 }
 
